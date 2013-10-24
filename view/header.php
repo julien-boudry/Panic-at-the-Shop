27510bcc12681cc75733feded45eb06b0e995be1
Boudry Julien <julien.boudry@supinternet.fr>
2013-10-24 15:44:56 +0200

    Logos app store

--- view/header.php
+++ view/header.php
@@ -5,8 +5,13 @@
 		Black Friday Run
 	</h1>
 	
+	<div id="coming-box">
+		<img class="coming" src="img/appstore_coming.png" alt="Coming soon App Store">
+		<img class="coming" src="img/android_coming.png" alt="Coming soon Android">
+	</div>
+	
 	<nav>
-		<span id="page_game" class="menu_button">The Game</span> | <span id="page_score" class="menu_button">The scoreboard</span> | <span id="page_about" class="menu_button">About US</span>
+		<span id="page_game" class="menu_button"><a href="#">The Game</a></span> | <span id="page_score" class="menu_button"><a href="#">The scoreboard</a></span> | <span id="page_about" class="menu_button"><a href="#">About US</a></span>
 	</nav>
 
 </header>
